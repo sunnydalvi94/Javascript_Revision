2f7bd3ccb67ce2319c1d59e32e3d6b8b8860dbe9
sunnydalvi94 <55959027+sunnydalvi94@users.noreply.github.com>
2024-07-20 17:46:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Javascript_Revision
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sunnydalvi94/Javascript_Revision)
---
+++ main.js
@@ -0,0 +1,18 @@
+import './style.css';
+// synchronous functionality
+
+function synchronousfun() {
+  console.log('start');
+  console.log('middle');
+  console.log('end');
+}
+synchronousfun();
+
+// Asynchronous functionality
+
+console.log('start');
+setTimeout(function () {
+  console.log('asychronous operation completed');
+}, 2000);
+console.log('end');
+
