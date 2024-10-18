6ab0b16e4c933fdb4cd950a1d13310518a73c720
sunny dalvi <55959027+sunnydalvi94@users.noreply.github.com>
2024-10-18 13:08:17 +0000

    datatype

---
+++ 01_basics/basics1.js
@@ -0,0 +1,20 @@
+let accid =12323;
+const city = "Jaipur";
+let year = 2023;
+
+console.log([accid,city,year]);
+
+
+/*
+prefer not use var
+due to block scope and function
+*/
+
+
+/*
+we are using node js not brower
+alert() 
+document.getelement will not work here.
+*/
+
+console.log(3+3)  // no need to use of ;  sentence terminator.
---
+++ 01_basics/datatype.js
@@ -0,0 +1,19 @@
+let name = "sunny"
+let age = "18"
+let islogged = false
+
+/*
+
+1.number
+2.bigint
+3.string
+4.boolen
+5.null  --> standalone value ;ex:- api temp value ; intially should not be 0, because it is value; so we put "null". so it neither 0 or empty.
+6.undefined  --> when value is not assigned
+7.symbol -- use in react, use for uniqueness in component.
+8.object
+*/
+
+
+console.log(typeof null)
+console.log(typeof undefined)
